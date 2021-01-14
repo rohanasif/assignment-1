c8576c9b5d797f15c3f2f15e49594679761670a6
Rohan <rohan.asif@arbisoft.com>
2021-01-14 16:25:09 +0500

    changed input_data.py

--- covid analyzer/input_data.py
+++ covid analyzer/input_data.py
@@ -1,6 +1,6 @@
 import csv
 line = []
-input_file = csv.DictReader(open("covid_cases_stats.csv"))
+input_file = csv.DictReader(open("covid_safety_measures.csv"))
 
 for line in input_file:
     print(line)
