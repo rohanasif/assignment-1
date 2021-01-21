3f5f102808b751de63a20e8cdfceff6701d8ec64
rohanasif <rohan.asif@arbisoft.com>
2021-01-21 14:29:09 +0500

    part a complete

--- covid analyzer/covidanalyzer.py
+++ covid analyzer/covidanalyzer.py
@@ -3,20 +3,22 @@ import argparse
 from FileReader import FileReader
 
 # Create the parser
-# parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser()
 # Add an argument
-# parser.add_argument('-a', type=str, required=True)
+parser.add_argument('-a', type=str, required=True)
 # Parse the argument
-# args = parser.parse_args()
+args = parser.parse_args()
+
 file_reader = FileReader()
 cases_dict = file_reader.covid_cases_reader("covid_cases_stats.csv")
 recovered_cases = ([case.total_recovered for case in cases_dict])
 total_cases = ([case.total_cases for case in cases_dict])
 names = ([case.name for case in cases_dict])
-country_input = input("Enter the name of a country: ")
+country_input = args.a
+print(country_input)
 required_index = names.index(country_input)
 required_ratio = int(recovered_cases[required_index])/int(total_cases[required_index])
-print("The required ratio is ", required_ratio)
+print(required_ratio)
 
 
 # measures_reader = FileReader()
