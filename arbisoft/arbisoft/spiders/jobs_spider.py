78c8081be16d87be147413799c4227ccc3a6de22
rohanasif <rohan.asif@arbisoft.com>
2021-02-03 16:08:42 +0500

    all values extracted in consecutive lists, need to print one by one in next step

--- arbisoft/arbisoft/spiders/jobs_spider.py
+++ arbisoft/arbisoft/spiders/jobs_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+import datetime
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
@@ -7,8 +8,50 @@ class JobsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        # job_titles = response.css(".storylink::text")[i].extract().lower().split("hiring")[1] # for job titles
-        # company_links = response.css(".sitestr::text").extract() # for company site (ycombinator links missing)
-        # job_links = response.css(".storylink ::attr(href)").extract() # for job sites (ycombinator jobs have id)
-        date_posted = response.css(".age a::text").extract()  # for date posted, datetime.datetime.now()-datetime.timedelta(days_ago))
-        yield {"date_posted" : date_posted}d
+        # Job Titles Code:
+        job_titles = response.css(".storylink::text").getall()
+        mylist = []
+        for item in job_titles:
+            if 'hiring ' in item:
+                item = item.split("hiring ")[1]
+                if item.startswith('– '):
+                    item = item.replace('– ', '')
+            elif 'Hiring ' in item:
+                item = item.split("Hiring ")[1]
+                if item.startswith('– '):
+                    item = item.replace('– ', '')
+            mylist.append(item)
+        print(mylist)
+
+        # Company Links Code:
+        company_links = company_links = response.css(".sitestr::text").getall()
+        print(company_links)
+
+        # Job Links Code:
+        job_links = response.css(".storylink ::attr(href)").getall()
+        ycombinatorlink = 'https://news.ycombinator.com/'
+        links = []
+        for item in job_links:
+            if 'http' not in item:
+                item = ycombinatorlink + item
+            links.append(item)
+        print(links)
+
+        # Time Ago Code:
+        time_ago = response.css(".age a::text").getall()
+        dates = []
+        for item in time_ago:
+            if 'days' in item or 'day' in item:
+                dates.append(str(datetime.date.today()-datetime.timedelta(days=int(item[:2]))))
+            else:
+                full_date = datetime.datetime.now()-datetime.timedelta(hours=int(item[:2]))
+                simple_date = full_date.date()
+                dates.append(str(simple_date))
+        print(dates)
+
+
+# job_titles = response.css(".storylink::text")[i].getall().lower().split("hiring")[1] # for job titles
+# company_links = response.css(".sitestr::text").getall() # for company site (ycombinator links missing)
+# job_links = response.css(".storylink ::attr(href)").getall() # for job sites (ycombinator jobs have id(RESOLVED))
+# time_ago = response.css(".age a::text").getall()  # datetime.datetime.now()-datetime.timedelta(days_ago)) gives the
+# posted date
