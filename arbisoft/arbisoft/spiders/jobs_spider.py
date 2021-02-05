f85c3dbc8362637b4744c28977e46290978d8323
rohanasif <rohan.asif@arbisoft.com>
2021-02-05 14:47:29 +0500

    help require on building temporary list code

--- arbisoft/arbisoft/spiders/jobs_spider.py
+++ arbisoft/arbisoft/spiders/jobs_spider.py
@@ -8,9 +8,10 @@ class JobsSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+
         # Job Titles Code:
         job_titles = response.css(".storylink::text").getall()
-        mylist = []
+        titles = []
         for item in job_titles:
             if 'hiring ' in item:
                 item = item.split("hiring ")[1]
@@ -20,12 +21,24 @@ class JobsSpider(scrapy.Spider):
                 item = item.split("Hiring ")[1]
                 if item.startswith('– '):
                     item = item.replace('– ', '')
-            mylist.append(item)
-        print(mylist)
+            titles.append(item)
+
+        # Temporary list:
+        full_jobs = response.css("tr.athing")
+        main_site_list = []
+        for item in full_jobs.getall():
+            if "sitebit" in item:
+                main_site_list.insert(full_jobs.getall().index(item), item)
+            else:
+                main_site_list.insert(full_jobs.getall().index(item), "")
+
+
+        #print(len(full_jobs))
+        #print(full_jobs)
+        print(main_site_list)
 
         # Company Links Code:
-        company_links = company_links = response.css(".sitestr::text").getall()
-        print(company_links)
+        company_links = response.css(".sitestr::text").getall()
 
         # Job Links Code:
         job_links = response.css(".storylink ::attr(href)").getall()
@@ -35,7 +48,6 @@ class JobsSpider(scrapy.Spider):
             if 'http' not in item:
                 item = ycombinatorlink + item
             links.append(item)
-        print(links)
 
         # Time Ago Code:
         time_ago = response.css(".age a::text").getall()
@@ -47,7 +59,6 @@ class JobsSpider(scrapy.Spider):
                 full_date = datetime.datetime.now()-datetime.timedelta(hours=int(item[:2]))
                 simple_date = full_date.date()
                 dates.append(str(simple_date))
-        print(dates)
 
 
 # job_titles = response.css(".storylink::text")[i].getall().lower().split("hiring")[1] # for job titles
