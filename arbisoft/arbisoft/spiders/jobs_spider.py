ffdea80923b97aa00d5393d43e995338ce568ea0
rohanasif <rohan.asif@arbisoft.com>
2021-02-09 18:59:35 +0500

    - fix truthy and falsy - changed title and all variable names - used yield instead of print

--- arbisoft/arbisoft/spiders/jobs_spider.py
+++ arbisoft/arbisoft/spiders/jobs_spider.py
@@ -11,10 +11,10 @@ class JobsSpider(scrapy.Spider):
     def parse(self, response):
 
         items = response.css("tr.athing, td.subtext")
-        all_job_titles = []
-        all_company_urls = []
-        all_job_urls = []
-        all_job_posting_dates = []
+        job_titles = []
+        company_urls = []
+        job_urls = []
+        job_posting_dates = []
         for item in items:
 
             job_title = item.css("td.title a.storylink::text").get()
@@ -22,7 +22,7 @@ class JobsSpider(scrapy.Spider):
             job_url = item.css("td.title a.storylink ::attr(href)").get()
             job_posting_date = item.css("span.age a::text").get()
 
-            if job_title is not None:
+            if job_title:
                 if 'hiring ' in job_title:
                     job_title = job_title.split("hiring ")[1]
                     if job_title.startswith('– '):
@@ -31,25 +31,25 @@ class JobsSpider(scrapy.Spider):
                     job_title = job_title.split("Hiring ")[1]
                     if job_title.startswith('– '):
                         job_title = job_title.replace('– ', '')
-                all_job_titles.append(job_title)
-                all_company_urls.append(company_url or '')
+                job_titles.append(job_title)
+                company_urls.append(company_url or '')
 
-            if job_url is not None:
+            if job_url:
                 ycombinatorlink = 'https://news.ycombinator.com/'
                 if 'http' not in job_url:
                     job_url = ycombinatorlink + job_url
-                all_job_urls.append(job_url)
+                job_urls.append(job_url)
 
-            if job_posting_date is not None:
+            if job_posting_date:
                 if 'days' in job_posting_date or 'day' in job_posting_date:
-                    all_job_posting_dates.append(str(datetime.date.today() -
+                    job_posting_dates.append(str(datetime.date.today() -
                                                      datetime.timedelta(days=int(job_posting_date[:2]))))
                 else:
                     full_date = datetime.datetime.now() - datetime.timedelta(hours=int(job_posting_date[:2]))
                     simple_date = full_date.date()
-                    all_job_posting_dates.append(str(simple_date))
+                    job_posting_dates.append(str(simple_date))
 
-        all_jobs_zip_object = zip(all_job_titles, all_company_urls, all_job_urls, all_job_posting_dates)
-        all_jobs_list = list(all_jobs_zip_object)
-        print(all_jobs_list)
-        print(len(all_jobs_list))
+        jobs_zip_object = zip(job_titles, company_urls, job_urls, job_posting_dates)
+        jobs_list = list(jobs_zip_object)
+        yield jobs_list
+
