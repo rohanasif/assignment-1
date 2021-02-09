392d4881b4cca54199690de88d3f30d3296e38d3
rohanasif <rohan.asif@arbisoft.com>
2021-02-09 14:57:31 +0500

    complete working code

--- arbisoft/arbisoft/spiders/jobs_spider.py
+++ arbisoft/arbisoft/spiders/jobs_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from ..items import ArbisoftItem
+import datetime
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
@@ -9,85 +9,46 @@ class JobsSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        things = ArbisoftItem()
-
         items = response.css("tr.athing, td.subtext")
-        # main_list=[]
+        all_job_titles = []
+        all_company_urls = []
+        all_job_urls = []
+        all_job_posting_dates = []
         for item in items:
 
             job_title = item.css("td.title a.storylink::text").get()
             company_url = item.css("td.title span.sitebit.comhead a span.sitestr::text").get()
             job_url = item.css("td.title a.storylink ::attr(href)").get()
             job_posting_date = item.css("span.age a::text").get()
+
             if job_title is not None:
-                # main_list.append(job_title)
-                # main_list.append(company_url or '')
-                things['job_title'] = job_title
-                things['company_url'] = company_url
+                if 'hiring ' in job_title:
+                    job_title = job_title.split("hiring ")[1]
+                    if job_title.startswith('– '):
+                        job_title = job_title.replace('– ', '')
+                elif 'Hiring ' in job_title:
+                    job_title = job_title.split("Hiring ")[1]
+                    if job_title.startswith('– '):
+                        job_title = job_title.replace('– ', '')
+                all_job_titles.append(job_title)
+                all_company_urls.append(company_url or '')
+
             if job_url is not None:
-                # main_list.append(job_url)
-                things['job_url'] = job_url
+                ycombinatorlink = 'https://news.ycombinator.com/'
+                if 'http' not in job_url:
+                    job_url = ycombinatorlink + job_url
+                all_job_urls.append(job_url)
+
             if job_posting_date is not None:
-                # main_list.append(job_posting_date)
-                things['job_posting_date'] = job_posting_date
+                if 'days' in job_posting_date or 'day' in job_posting_date:
+                    all_job_posting_dates.append(str(datetime.date.today() -
+                                                     datetime.timedelta(days=int(job_posting_date[:2]))))
+                else:
+                    full_date = datetime.datetime.now() - datetime.timedelta(hours=int(job_posting_date[:2]))
+                    simple_date = full_date.date()
+                    all_job_posting_dates.append(str(simple_date))
 
-            # things['job_title'] = job_title
-            # things['company_url'] = company_url
-            # things['job_url'] = job_url
-            # things['job_posting_date'] = job_posting_date
-
-            yield things
-
-            print('done')
-        # Job Titles Code:
-        # job_titles = response.css(".storylink::text").getall()
-        # titles = []
-        # for item in job_titles:
-        #     if 'hiring ' in item:
-        #         item = item.split("hiring ")[1]
-        #         if item.startswith('– '):
-        #             item = item.replace('– ', '')
-        #     elif 'Hiring ' in item:
-        #         item = item.split("Hiring ")[1]
-        #         if item.startswith('– '):
-        #             item = item.replace('– ', '')
-        #     titles.append(item)
-
-        # Company Links Code:
-        # full_jobs = response.css("tr.athing")
-        # main_site_list = []
-        # for item in full_jobs:
-        #     company_link=item.css(".sitestr::text").get()
-        #     main_site_list.append(company_link or "")
-
-        # Job Links Code:
-        # job_links = response.css(".storylink ::attr(href)").getall()
-        # ycombinatorlink = 'https://news.ycombinator.com/'
-        # links = []
-        # for item in job_links:
-        #     if 'http' not in item:
-        #         item = ycombinatorlink + item
-        #     links.append(item)
-
-        # Time Ago Code:
-        # time_ago = response.css(".age a::text").getall()
-        # dates = []
-        # for item in time_ago:
-        #     if 'days' in item or 'day' in item:
-        #         dates.append(str(datetime.date.today()-datetime.timedelta(days=int(item[:2]))))
-        #     else:
-        #         full_date = datetime.datetime.now()-datetime.timedelta(hours=int(item[:2]))
-        #         simple_date = full_date.date()
-        #         dates.append(str(simple_date))
-
-        # For Next Page:
-        # ycombinatorlink = 'https://news.ycombinator.com/'
-        # next_page = ycombinatorlink + response.css("a.morelink::attr(href)")
-        # if next_page:
-        #     yield response.follow(next_page, callback=self.parse)
-
-# job_titles = response.css(".storylink::text")[i].getall().lower().split("hiring")[1] # for job titles
-# company_links = response.css(".sitestr::text").getall() # for company site (ycombinator links missing)
-# job_links = response.css(".storylink ::attr(href)").getall() # for job sites (ycombinator jobs have id(RESOLVED))
-# time_ago = response.css(".age a::text").getall()  # datetime.datetime.now()-datetime.timedelta(days_ago)) gives the
-# posted date
+        all_jobs_zip_object = zip(all_job_titles, all_company_urls, all_job_urls, all_job_posting_dates)
+        all_jobs_list = list(all_jobs_zip_object)
+        print(all_jobs_list)
+        print(len(all_jobs_list))
