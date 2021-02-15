eb9279e2531937a1fd8f8632a7cd584dfc1bddae
rohanasif <rohan.asif@arbisoft.com>
2021-02-15 17:12:34 +0500

    seperate methods implement

--- arbisoft/arbisoft/spiders/jobs_spider.py
+++ arbisoft/arbisoft/spiders/jobs_spider.py
@@ -16,19 +16,12 @@ class JobsSpider(scrapy.Spider):
         jobitems = response.css("tr.athing")
         items = zip(times, jobitems)
         ycombinatorlink = 'https://news.ycombinator.com/'
-        for time, jobitem in items:
-            job = JobItem()
-            job_title = jobitem.css("a.storylink::text").get()
-            company_url = jobitem.css("span.sitebit.comhead a span.sitestr::text").get()
-            job_url = jobitem.css("a.storylink ::attr(href)").get()
-            job_posting_date = time.css("a::text").get()
-            if job_posting_date:
-                if 'days' in job_posting_date and int(job_posting_date[:2]) > 5:
-                    break
+
+        def generate_posting_dates(job_posting_date):
             if job_posting_date:
                 if 'day' in job_posting_date:
                     job['job_posting_date'] = str(datetime.date.today() -
-                                                 datetime.timedelta(days=int(job_posting_date[:2])))
+                                                  datetime.timedelta(days=int(job_posting_date[:2])))
                 elif 'hour' in job_posting_date:
                     full_date = datetime.datetime.now() - datetime.timedelta(hours=int(job_posting_date[:2]))
                     simple_date = full_date.date()
@@ -41,7 +34,9 @@ class JobsSpider(scrapy.Spider):
                     full_date = datetime.datetime.now() - datetime.timedelta(seconds=int(job_posting_date[:2]))
                     simple_date = full_date.date()
                     job['job_posting_date'] = str(simple_date)
+            return job['job_posting_date']
 
+        def generate_job_titles(job_title):
             if job_title:
                 if 'hiring' in job_title or 'Hiring' in job_title:
                     if job_title.endswith('hiring') or job_title.endswith('Hiring'):
@@ -53,13 +48,34 @@ class JobsSpider(scrapy.Spider):
                             job['job_title'] = match[0]
                 else:
                     job['job_title'] = job_title
-            job['company_url'] = company_url or ''
+            return job['job_title']
 
+        def generate_company_urls(company_url):
+            job['company_url'] = company_url or ''
+            return job['company_url']
+
+        def generate_job_urls(job_url):
             if job_url:
                 if 'http' not in job_url:
                     job_url = ycombinatorlink + job_url
                 job['job_url'] = job_url
+            return job['job_url']
+
+        for time, jobitem in items:
+            job = JobItem()
+            job_title = jobitem.css("a.storylink::text").get()
+            company_url = jobitem.css("span.sitebit.comhead a span.sitestr::text").get()
+            job_url = jobitem.css("a.storylink ::attr(href)").get()
+            job_posting_date = time.css("a::text").get()
+            if job_posting_date:
+                if 'days' in job_posting_date and int(job_posting_date[:2]) > 5:
+                    break
+            generate_posting_dates(job_posting_date)
+            generate_job_titles(job_title)
+            generate_company_urls(company_url)
+            generate_job_urls(job_url)
             yield job
+
         next_page = response.css("a.morelink::attr(href)").get()
         if next_page and job_posting_date:
             if 'days' in job_posting_date and int(job_posting_date[:2]) < 5:
